f382084c0d95e63b8ae465161905ffc63edfad3b
Anna Kadomtseva <abanyaaleksandrovna@gmail.com>
2020-10-28 20:17:56 +0300

    Сделала перемещение метки по карте

--- js/map.js
+++ js/map.js
@@ -2,14 +2,31 @@
 
 (function () {
   const MainPinSize = {
-    WIDTH: 62,
-    HEIGHT: 62,
+    WIDTH: 65,
+    HEIGHT: 65,
     ARROW: 22
   };
 
+  const PIN_SIZE_WIDTH_ARROW = MainPinSize.HEIGHT + MainPinSize.ARROW;
+
+  const DefaultMainPin = {
+    X: 600,
+    Y: 375
+  };
+
+  const IntervalCoordinates = {
+    X_MIN: 0,
+    X_MAX: 1200,
+    Y_MIN: 130,
+    Y_MAX: 630
+  };
+
   const mapPinMain = document.querySelector('.map__pin--main');
-  let mainPinLeft = getComputedStyle(mapPinMain).left;
-  let mainPinTop = getComputedStyle(mapPinMain).top;
+  const address = document.querySelector('#address');
+
+  let setAddress = function (x, y) {
+    address.value = x + ', ' + y;
+  };
 
   const errorHandler = function (errorMessage) {
     const error = document.querySelector('#error');
@@ -23,49 +40,67 @@
     document.main.insertAdjacentElement('afterbegin', error);
   };
 
-  let addressDefaultCoords = {
-    left: parseInt(mainPinLeft, 10),
-    top: parseInt(mainPinTop, 10),
-    offsetY: parseInt((MainPinSize.HEIGHT / 2 + MainPinSize.ARROW), 10)
+  const setDefaultCoordinates = function (isActivePage) {
+    const deactivatedX = DefaultMainPin.X + Math.floor(MainPinSize.WIDTH / 2);
+    const deactivatedY = DefaultMainPin.Y + Math.floor(MainPinSize.HEIGHT / 2);
+    const activatedY = DefaultMainPin.Y + PIN_SIZE_WIDTH_ARROW;
+
+    if (isActivePage) {
+      setAddress(deactivatedX, activatedY);
+    } else {
+      setAddress(deactivatedX, deactivatedY);
+    }
   };
 
-  window.form.setAddress(addressDefaultCoords.left, addressDefaultCoords.top);
+  setDefaultCoordinates();
+
+  const setActualCoordinates = function () {
+    const changedX = parseInt(mapPinMain.style.left, 10) + Math.floor(MainPinSize.WIDTH / 2);
+    const changedY = parseInt(mapPinMain.style.top, 10) + PIN_SIZE_WIDTH_ARROW;
+
+    setAddress(changedX, changedY);
+  };
 
   const activateFields = function () {
     window.card.mapElement.classList.remove('map--faded');
-    window.form.activateForm();
-    window.form.setAddress(addressDefaultCoords.left, addressDefaultCoords.offsetY);
     window.backend.load(window.pin.render, errorHandler);
-    mapPinMain.removeEventListener('mousedown', onMainPinActivate);
+    window.form.activateForm();
+    setDefaultCoordinates(true);
   };
-
-  const onMainPinActivate = function (evt) {
-    window.utils.isMouseButtonEvent(evt, activateFields);
-  };
-
-  mapPinMain.addEventListener('mousedown', onMainPinActivate);
 
   const onMainPinMouseDown = function (evt) {
     evt.preventDefault();
+    window.utils.isMouseButtonEvent(evt, activateFields);
 
     let startCoords = {
-      x: evt.clientX,
-      y: evt.clientY
+      x: evt.clientX - mapPinMain.getBoundingClientRect().left,
+      y: evt.clientY - mapPinMain.getBoundingClientRect().top
     };
 
     const onMouseMove = function (moveEvt) {
       moveEvt.preventDefault();
 
       let shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY
+        x: moveEvt.clientX - startCoords.x - window.card.mapElement.getBoundingClientRect().left,
+        y: moveEvt.clientY - startCoords.y - window.card.mapElement.getBoundingClientRect().top
       };
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
-      mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
-      mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
+
+      if (shift.y < IntervalCoordinates.Y_MIN - PIN_SIZE_WIDTH_ARROW) {
+        shift.y = IntervalCoordinates.Y_MIN - PIN_SIZE_WIDTH_ARROW;
+      } else if (shift.y > IntervalCoordinates.Y_MAX - PIN_SIZE_WIDTH_ARROW) {
+        shift.y = IntervalCoordinates.Y_MAX - PIN_SIZE_WIDTH_ARROW;
+      }
+
+      if (shift.x < IntervalCoordinates.X_MIN + Math.floor(MainPinSize.WIDTH / 2)) {
+        shift.x = IntervalCoordinates.X_MIN - Math.floor(MainPinSize.WIDTH / 2);
+      } else if (shift.x > IntervalCoordinates.X_MAX - Math.floor(MainPinSize.WIDTH / 2)) {
+        shift.x = IntervalCoordinates.X_MAX - Math.floor(MainPinSize.WIDTH / 2);
+      }
+
+      mapPinMain.style.left = shift.x + 'px';
+      mapPinMain.style.top = shift.y + 'px';
+
+      setActualCoordinates();
     };
 
     const onMouseUp = function (upEvt) {
@@ -85,5 +120,4 @@
 
   mapPinMain.addEventListener('mousedown', onMainPinMouseDown);
   mapPinMain.addEventListener('keydown', onMainPinEnterPress);
-
 })();
